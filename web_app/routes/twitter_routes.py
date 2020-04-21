1e81186c973311203fe291a6a43cb220c5b7ffac
Mike Rossetti <s2t2@users.noreply.github.com>
2020-04-21 14:05:37 -0400

    Migrate user and tweet tables

--- web_app/routes/twitter_routes.py
+++ web_app/routes/twitter_routes.py
@@ -13,4 +13,9 @@ def fetch_user_data(screen_name=None):
     api = api_client()
     user = api.get_user(screen_name)
     statuses = api.user_timeline(screen_name, tweet_mode="extended", count=150, exclude_replies=True, include_rts=False)
+
+
+    # store them in the database
+
+
     return jsonify({"user": user._json, "tweets_count": len(statuses)})
