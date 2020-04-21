09bfd08c43a73f10e1b8621e98a8abb4e9294698
Mike Rossetti <s2t2@users.noreply.github.com>
2020-04-21 13:58:13 -0400

    Setup twitter routes, refactor twitter service to facilitate imports

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -5,6 +5,7 @@ from flask import Flask
 from web_app.models import db, migrate
 from web_app.routes.home_routes import home_routes
 from web_app.routes.book_routes import book_routes
+from web_app.routes.twitter_routes import twitter_routes
 
 DATABASE_URI = "sqlite:////Users/mjr/Desktop/twitoff-13/twitoff_13.db" # TODO: read from env var
 SECRET_KEY = "super secret" # TODO: read from env var
@@ -22,6 +23,7 @@ def create_app():
     # configure routes:
     app.register_blueprint(home_routes)
     app.register_blueprint(book_routes)
+    app.register_blueprint(twitter_routes)
 
     return app
 
---
+++ web_app/routes/twitter_routes.py
@@ -0,0 +1,16 @@
+# web_app/routes/twitter_routes.py
+
+from flask import Blueprint, jsonify
+
+from web_app.services.twitter_service import api_client
+
+twitter_routes = Blueprint("twitter_routes", __name__)
+
+@twitter_routes.route("/users/<screen_name>/fetch")
+def fetch_user_data(screen_name=None):
+    print(screen_name)
+
+    api = api_client()
+    user = api.get_user(screen_name)
+    statuses = api.user_timeline(screen_name, tweet_mode="extended", count=150, exclude_replies=True, include_rts=False)
+    return jsonify({"user": user._json, "tweets_count": len(statuses)})
