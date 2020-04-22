b6f176dbd20fc1ba542fbf365fb0c31cfb456210
Mike Rossetti <s2t2@users.noreply.github.com>
2020-04-22 14:14:41 -0400

    Make prediction

--- web_app/routes/stats_routes.py
+++ web_app/routes/stats_routes.py
@@ -4,6 +4,9 @@ from flask import Blueprint, render_template, request
 from sklearn.datasets import load_iris
 from sklearn.linear_model import LogisticRegression
 
+from web_app.models import User
+from web_app.services.basilica_service import connection as basilica_connection
+
 stats_routes = Blueprint("stats_routes", __name__)
 
 @stats_routes.route("/stats/iris")
@@ -26,10 +29,36 @@ def twitoff_prediction():
     screen_name_b = request.form["screen_name_b"]
     tweet_text = request.form["tweet_text"]
 
-    # todo: train model
+    #
+    # TRAIN THE MODEL
+    #
+    # inputs: embeddings for each tweet
+    # labels: screen name for each tweet
 
-    # todo: make prediction
-    screen_name_most_likely = "TODO"
+    model = LogisticRegression()
+
+    user_a = User.query.filter(User.screen_name == screen_name_a).one()
+    user_b = User.query.filter(User.screen_name == screen_name_b).one()
+
+    user_a_tweets = user_a.tweets
+    user_b_tweets = user_b.tweets
+
+    embeddings = []
+    labels = []
+    all_tweets = user_a_tweets + user_b_tweets
+    for tweet in all_tweets:
+        embeddings.append(tweet.embedding)
+        labels.append(tweet.user.screen_name)
+
+    model.fit(embeddings, labels)
+
+    #
+    # MAKE PREDICTION
+    #
+
+    example_embedding = basilica_connection.embed_sentence(tweet_text)
+    result = model.predict([example_embedding])
+    screen_name_most_likely = result[0]
 
     return render_template("prediction_results.html",
         screen_name_a=screen_name_a,
