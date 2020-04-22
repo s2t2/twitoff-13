cbd1ef5e3eb4e94ed0f0ca74fbd9dd4e775de4ff
Mike Rossetti <s2t2@users.noreply.github.com>
2020-04-22 13:52:23 -0400

    Prediction routing and nav

--- web_app/routes/stats_routes.py
+++ web_app/routes/stats_routes.py
@@ -1,6 +1,6 @@
 
 
-from flask import Blueprint
+from flask import Blueprint, render_template, request
 from sklearn.datasets import load_iris
 from sklearn.linear_model import LogisticRegression
 
@@ -14,3 +14,26 @@ def iris():
     # make a prediction:
     results = str(clf.predict(X[:2, :]))
     return results
+
+@stats_routes.route("/")
+def twitoff_prediction_form():
+    return render_template("prediction_form.html")
+
+@stats_routes.route("/stats/predict", methods=["POST"])
+def twitoff_prediction():
+    print("FORM DATA:", dict(request.form))
+    screen_name_a = request.form["screen_name_a"]
+    screen_name_b = request.form["screen_name_b"]
+    tweet_text = request.form["tweet_text"]
+
+    # todo: train model
+
+    # todo: make prediction
+    screen_name_most_likely = "TODO"
+
+    return render_template("prediction_results.html",
+        screen_name_a=screen_name_a,
+        screen_name_b=screen_name_b,
+        tweet_text=tweet_text,
+        screen_name_most_likely=screen_name_most_likely
+    )
